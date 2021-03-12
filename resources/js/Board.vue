30d482547ffaf3a16c059c94f720ac1ac9cfc98f
Joao <38017770+joaovictor74@users.noreply.github.com>
2021-03-12 17:28:54 -0300

    Corrigindo pasta raiz
    -Criando a pagina Boards

---
+++ resources/js/Board.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="h-full flex flex-col items-stretch bg-purple-500">
+        <div class="text-white flex justify-between items-center mb-2 bg-purple-600 ">
+            <div class="ml-2 w-1/3">x</div>
+            <div class="text-lg opacity-50 cursor-pointer hover:bg-opacity-75">Laravello</div>
+            <div class="mr-2 w-1/3 flex justify-end">x</div>
+        </div>
+        <div class="h-full flex flex-1 flex-col items-stretch">
+            <div class="mx-4 mb-2 text-white font-bold text-lg">The Board title</div>
+            <div class="flex flex-1 items-start overflow-x-auto mx-2">
+                <div class="bg-gray-300 rounded-sm p-2 mr-2 list">
+                    <div class="flex justify-between">
+                        <div class="text-gray-800 pl-2 pb-2 font-bold">List Title</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .header{
+        height:40px;
+    }
+    .list{
+        width:250px;
+    }
+</style>
+<script>
+export default {
+    
+}
+</script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,9 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.postCss('resources/css/app.css','public/css',[
-   require('tailwindcss')
-]);
-mix.js('resources/js/app.js', 'public/js');
-   
+
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css',[
+      require('tailwindcss')
+   ]);
